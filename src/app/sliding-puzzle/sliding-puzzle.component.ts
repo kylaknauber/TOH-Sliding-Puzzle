31ceac4e35b8d29d272c7579215c7458b6b91a25
kylaknauber <kylaknauber@gmail.com>
2025-09-24 21:05:12 -0400

    cleaning up and modifying sliding puzzle logic and css

--- src/app/sliding-puzzle/sliding-puzzle.component.ts
+++ src/app/sliding-puzzle/sliding-puzzle.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {CdkDrag, CdkDragDrop, DragDropModule, transferArrayItem} from '@angular/cdk/drag-drop';
-import {NgForOf, NgIf, NgStyle} from "@angular/common";
+import {CdkDrag, CdkDragDrop, DragDropModule, transferArrayItem, CdkDragEnd, CdkDragRelease} from '@angular/cdk/drag-drop';
+import {NgForOf, NgIf, NgStyle, NgFor} from "@angular/common";
 import {RouterOutlet, RouterLink, ActivatedRoute} from "@angular/router";
 //import {ActivatedRoute} from "@angular/router";
 
@@ -9,6 +9,7 @@ import {RouterOutlet, RouterLink, ActivatedRoute} from "@angular/router";
   standalone: true,
   imports: [
     NgForOf,
+    NgFor,
     NgIf,
     NgStyle,
     DragDropModule,
@@ -20,9 +21,8 @@ import {RouterOutlet, RouterLink, ActivatedRoute} from "@angular/router";
   styleUrl: './sliding-puzzle.component.css'
 })
 export class SlidingPuzzleComponent {
-
-
-  block1: string[] = [];
+  /**
+   * block1: string[] = [];
   block2: string[] = [];
   block3: string[] = [];
   block4: string[] = [];
@@ -39,6 +39,9 @@ export class SlidingPuzzleComponent {
   block15: string[] = [];
   block16: string[] = [];
   shuffledNumbers: string[] = [];
+   */
+
+  blocks: string[] = [];
 
   isWinner: boolean = false;
 
@@ -47,7 +50,8 @@ export class SlidingPuzzleComponent {
   }
 
   shuffle() {
-    this.block1 = [];
+    /**
+     * this.block1 = [];
     this.block2 = [];
     this.block3 = [];
     this.block4 = [];
@@ -63,18 +67,23 @@ export class SlidingPuzzleComponent {
     this.block14 = [];
     this.block15 = [];
     this.block16 = [];
+     */
 
-    //array of string numbers
-    let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
-    this.shuffledNumbers = [];
-    let index;
-    while (numbers.length > 0) {
-      index = Math.floor(Math.random() * numbers.length);
-      this.shuffledNumbers.push(numbers[index]);
-      numbers.splice(index, 1);
+    let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", ""];
+    // Shuffle the array of numbers
+    for(let i = numbers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
     }
 
-    this.block1.push(this.shuffledNumbers[0]);
+    // Add the shuffled numbers to the blocks in the puzzle
+    this.blocks = [];
+    numbers.forEach((num) => {
+      this.blocks.push(num);
+    })
+
+    /**
+     * this.block1.push(this.shuffledNumbers[0]);
     this.block2.push(this.shuffledNumbers[1]);
     this.block3.push(this.shuffledNumbers[2]);
     this.block4.push(this.shuffledNumbers[3]);
@@ -89,10 +98,11 @@ export class SlidingPuzzleComponent {
     this.block13.push(this.shuffledNumbers[12]);
     this.block14.push(this.shuffledNumbers[13]);
     this.block15.push(this.shuffledNumbers[14]);
-    //this.block16.push(this.shuffledNumbers[15]);
+     */
   }
 
-  moveBlock(event: CdkDragDrop<string[]>) {
+  /**
+   * moveBlock(event: CdkDragDrop<string[]>) {
     if (event.container.data.length === 0) {
       transferArrayItem(
         event.previousContainer.data,
@@ -104,9 +114,64 @@ export class SlidingPuzzleComponent {
       console.log('cannot move objects where there already exists an object')
     }
 
-    if(this.block1[0] === "1" && this.block2[0]=== "2" && this.block3[0] === "3" && this.block4[0] === "4" && this.block5[0] === "5" && this.block6[0] === "6" && this.block7[0] === "7" && this.block8[0] === "8" && this.block9[0] === "9" && this.block10[0] === "10" && this.block11[0] === "11" && this.block12[0] === "12" && this.block13[0] === "13" && this.block14[0] === "14" && this.block15[0] === "15") {
+    if(this.isSolved()) {
       this.isWinner = true;
     }
+    else {
+      this.isWinner = false;
+    }
+
+    /**
+     * if(this.block1[0] === "1" && this.block2[0]=== "2" && this.block3[0] === "3" && this.block4[0] === "4" && this.block5[0] === "5" && this.block6[0] === "6" && this.block7[0] === "7" && this.block8[0] === "8" && this.block9[0] === "9" && this.block10[0] === "10" && this.block11[0] === "11" && this.block12[0] === "12" && this.block13[0] === "13" && this.block14[0] === "14" && this.block15[0] === "15") {
+      this.isWinner = true;
+    }
+     
+  }
+   * 
+   */
+
+  // Move the block
+  moveBlock(event: CdkDragDrop<any>) {
+    const blockToMove = event.item.data
+    const emptySpace = this.blocks.indexOf("");
+    
+    const movedBlockRow = Math.floor(blockToMove / 4);
+    const movedBlockColumn = blockToMove % 4;
+    const emptyBlockRow = Math.floor(emptySpace / 4);
+    const emptyBlockColumn = emptySpace % 4;
+
+    // Checking if the rows are the same and the columns are next to each other
+    // or if the columns are the same and the rows are next to each other.
+    // This would show that the empty space and block to move can be swapped
+    const isMovable = (movedBlockRow === emptyBlockRow && Math.abs(movedBlockColumn - emptyBlockColumn) === 1) ||
+    (movedBlockColumn === emptyBlockColumn && Math.abs(movedBlockRow - emptyBlockRow) === 1);
+
+    // Check if the block can be moved, then swap the indices
+    if(isMovable) {
+      [this.blocks[blockToMove], this.blocks[emptySpace]] = [this.blocks[emptySpace], this.blocks[blockToMove]];
+    }
+
+    // If the entire board is solved, show win screen
+    if(this.isSolved()) {
+      this.isWinner = true;
+    }
+
+  }
+
+  // Checks if the board is solved 
+  isSolved() {
+    let i = 0;
+    while(i < 15) {
+      if(this.blocks[i] !== (i + 1).toString()) {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+
+  resetGame() {
+    location.reload();
   }
 
 }
